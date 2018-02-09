e26ac90350c3d4da5ecaf974fdb2683d186a9d5f
Ilya <tefc@yandex.ru>
2018-02-09 14:52:45 +0300

    f123

---
+++ EmptyAspNetCore/Startup.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+
+namespace EmptyAspNetCore
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddProvider(new FileLoggerProvider("D://123.txt"));
+            app.Run(async (context) =>
+            {
+                // создаем объект логгера
+                var logger = loggerFactory.CreateLogger("RequestInfoLogger");
+                // пишем на консоль информацию
+                logger.LogInformation("Processing request {0}", context.Request.Path);
+
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+
+        public class FileLogger : ILogger
+        {
+            private string filePath;
+            private object _lock = new object();
+            public FileLogger(string path)
+            {
+                filePath = path;
+            }
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                return null;
+            }
+
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                //return logLevel == LogLevel.Trace;
+                return true;
+            }
+
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                if (formatter != null)
+                {
+                    lock (_lock)
+                    {
+                        File.AppendAllText(filePath, formatter(state, exception) + Environment.NewLine);
+                    }
+                }
+            }
+        }
+
+        public class FileLoggerProvider : ILoggerProvider
+        {
+            private string path;
+            public FileLoggerProvider(string _path)
+            {
+                path = _path;
+            }
+            public ILogger CreateLogger(string categoryName)
+            {
+                return new FileLogger(path);
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
---
+++ EmptyAspNetCore/TokenMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+public class TokenMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly string _anystr;
+
+    public TokenMiddleware(RequestDelegate next,string anystr)
+    {
+        this._anystr = anystr;
+        this._next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var token = context.Request.Query["token"];
+        if (_anystr != "12345678")
+        {
+            context.Response.StatusCode = 403;
+            await context.Response.WriteAsync("Token is invalid");
+        }
+        else
+        {
+            await _next.Invoke(context);
+        }
+    }
+}
+
+public static class TokenExtensions
+{
+    public static IApplicationBuilder UseToken(this IApplicationBuilder builder, string anystr)
+    {
+        return builder.UseMiddleware<TokenMiddleware>(anystr);
+    }
+}
